a5e4ed46cc7b5b26aa62f52cfb0ba35a71aabda7
nmougino <nmougino@student.42.fr>
2016-09-26 22:16:42 +0200

    Fix environment using

--- srcs/exe_fork.c
+++ srcs/exe_fork.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/24 21:45:42 by nmougino          #+#    #+#             */
-/*   Updated: 2016/09/26 20:22:01 by nmougino         ###   ########.fr       */
+/*   Updated: 2016/09/26 22:14:24 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,10 @@ static void	do_fork(char *exe, char **com, t_env *menv)
 	pid_t	pid;
 	char	**env;
 
-	menv = NULL;
 	pid = fork();
 	env = env_conv(menv);
 	if (!pid)
-		execve(exe, com, NULL);
+		execve(exe, com, env);
 	else
 	{
 		waitpid(pid, NULL, 0);
